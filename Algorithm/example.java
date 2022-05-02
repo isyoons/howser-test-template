0a615c9c254706922196e7da8a61d20827be98e7
ilseok Yoon <yis8574@nate.com>
2022-05-03 01:14:21 +0900

    Create example.java

---
+++ Algorithm/example.java
@@ -0,0 +1,23 @@
+// 1번문제
+class Solution {
+    public int solution(int[] nums) {
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
+
+// 2번문제
+class Solution {
+    public int solution(int[] nums) {
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
